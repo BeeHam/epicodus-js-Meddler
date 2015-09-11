63a4da93746befc973d407df71d65e9380f32ac7
Brigham <brigham.thompson@gmail.com>
2015-09-11 10:02:54 -0700

    add spec for replacing multiple words

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe("findReplace", function() {
   it("will find and replace one matching word", function() {
     expect(findReplace("hello world", "world", "universe")).to.equal("hello universe");
   })
+
+  it("will find and replace all matching words", function() {
+    expect(findReplace("hello hello", "hello", "goodbye")).to.equal("goodbye goodbye");
+  })
 });
