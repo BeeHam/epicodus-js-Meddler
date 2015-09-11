63c62f98d08703d691d1d0baf948ceb007e2ddf1
Brigham <brigham.thompson@gmail.com>
2015-09-11 10:55:45 -0700

    add html front-end a jquery interactivity

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ var findReplace = function(aString, unwanted, wanted) {
   var newString = aString.replace(newUnwanted, wanted);
   return newString;
 }
+
+$(document).ready(function() {
+  $("form.meddler").submit(function(event) {
+    var aString = $("input#string").val();
+    var unwanted = $("input#word").val();
+    var wanted = $("input#replacement").val();
+
+    $("#result").text(findReplace(aString, unwanted, wanted));
+
+    event.preventDefault();
+  });
+});
